ee65cf89fa784b7929c1898e8dcec491d92ef225
timdechant <timdechant.git@gmail.com>
2025-03-11 08:27:10 -0400

    code and visual cleanup

--- monty-hall/monty_hall_sim.py
+++ monty-hall/monty_hall_sim.py
@@ -53,8 +53,8 @@ def _(mo):
         whammy = mo.icon('game-icons:goat', size='60%', color='brown')
         prize = mo.icon('game-icons:cash', size='60%', color='gold')
         winner = mo.icon('game-icons:cash', size='60%', color='gold', style={'background-color':"#50C878"})
-        # loser = mo.icon('game-icons:goat', size='60%', color='brown', style={'background-color':"red"})
-        loser = mo.icon('game-icons:wooden-door', size='100%', color='#777777', style={'background-color':"red"})
+        loser = mo.icon('game-icons:goat', size='60%', color='brown', style={'background-color':"#ff6347"})
+        # loser = mo.icon('game-icons:wooden-door', size='100%', color='#777777', style={'background-color':"#ff6347"})
 
         @classmethod
         def to_dict(cls):
@@ -62,12 +62,7 @@ def _(mo):
 
         @classmethod
         def legend(cls):
-            md = mo.hstack(
-                [mo.vstack(item) for item in cls.to_dict().items()],
-                justify='center',
-                align='center',
-            )
-            return md
+            return mo.hstack(map(mo.vstack, cls.to_dict().items()), justify='center', align='center')
 
         @classmethod
         def test(cls):
@@ -85,43 +80,55 @@ def _(mo):
 def _(Icons, mo):
     import random
     class Game(object):
-        def __init__(self, change=False):
+        def __init__(self, count=3, change=False):
+            """
+            Remember game parameters, initialize historical results,
+            and set things in motion.
+            """
+            self._count = count
             self._change = change
             self._attempts = 0
             self._wins = 0
             self._next_step = self.start
 
-        def choose(self, choices):
-            idx = random.choice(choices)
-            return idx,list(i for i in choices if i != idx)
-
         def start(self):
-            # print('start')
-            self._prize,self._whammies = self.choose(range(3))
-            self._icons = [Icons.door]*3
-            self._selected = None
-            self._revealed = None
+            """
+            Set up the game board: show the doors and choose
+            which holds the prize (without revealing anything.)
+            """
+            self._prize = random.randrange(self._count)
+            self._icons = [Icons.door]*self._count
             self._next_step = self.step1
 
         def step1(self):
-            # print('step1')
-            self._selected,_ = self.choose(range(3))
+            """
+            Simulated user randomly selects a door,
+            independent of which holds the prize.
+            """
+            self._selected = random.randrange(self._count)
             self._icons[self._selected] = Icons.selected
             self._next_step = self.step2
 
         def step2(self):
-            # print('step2')
-            whammy_doors = [door for door in range(3) if door not in (self._prize,self._selected)]
-            self._revealed = random.choice(whammy_doors)
-            self._icons[self._revealed] = Icons.whammy
+            """
+            Reveal all but one of the bogus prizes.
+            """
+            occupied = (self._prize, self._selected)
+            reveal_options = [door for door in range(self._count) if door not in occupied]
+            self._revealed = random.sample(reveal_options, self._count-2)
+            for door in self._revealed:
+                self._icons[door] = Icons.whammy
             self._next_step = self.step3
 
         def step3(self):
+            """
+            Optionally, change user selection to the remaining door.
+            """
             if not self._change:
                 return self.step4()
-            # print('step3')
 
-            remaining_doors = [door for door in range(3) if door not in (self._selected,self._revealed)]
+            occupied = list(self._revealed) + [self._selected]
+            remaining_doors = [door for door in range(self._count) if door not in occupied]
             assert(len(remaining_doors)==1)
 
             self._icons[self._selected] = Icons.door
@@ -130,14 +137,18 @@ def _(Icons, mo):
             self._next_step = self.step4
 
         def step4(self):
-            # print('step4')
-            self._icons[self._prize] = Icons.prize
+            """
+            Reveal the selection, hightlighting win/loss.
+            """
+            self._icons[self._selected] = Icons.winner if self._selected == self._prize else Icons.loser
+            self._next_step = self.step5
+
+        def step5(self):
+            """
+            Update the historical results.
+            """
             if self._selected == self._prize:
-                self._icons[self._prize] = Icons.winner
                 self._wins += 1
-            else:
-                self._icons[self._prize] = Icons.prize
-                self._icons[self._selected] = Icons.loser
             self._attempts += 1
             self._next_step = self.start
 
@@ -147,13 +158,15 @@ def _(Icons, mo):
 
         @property
         def md(self):
-            return mo.vstack([
-                mo.center(
-                    mo.md(f'## Experiment results: {100*self._wins//self._attempts}% success' if self._attempts else '')
-                ),
-                mo.hstack([]),
-                mo.hstack(self._icons, justify='center', align='center'),
-            ])
+            """
+            Render the game board to markdown.
+            """
+            vstack = []
+            if self._attempts > 1:
+                success = 100*self._wins//self._attempts
+                vstack = [mo.center(mo.md(f'## After {self._attempts} games: {success}% success'))]
+            vstack.append(mo.hstack(self._icons, justify='center', align='center'))
+            return mo.vstack(vstack)
 
     game1 = Game()
     step1 = mo.ui.refresh(label='Step:', options=[5,1], default_interval=1)
@@ -161,7 +174,10 @@ def _(Icons, mo):
     game2 = Game(change=True)
     step2 = mo.ui.refresh(label='Step:', options=[5,1], default_interval=1)
 
-    return Game, game1, game2, random, step1, step2
+    game3 = Game(change=True, count=6)
+    step3 = mo.ui.refresh(label='Step:', options=[5,1], default_interval=1)
+
+    return Game, game1, game2, game3, random, step1, step2, step3
 
 
 @app.cell
@@ -180,6 +196,34 @@ def _(game2, mo, step2):
         step2,
         mo.md('# Policy: Always change choice.'),
         game2.step()
+    ])
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(f"""
+    # What is going on?
+
+    ## Allow the games to run for at least 10~25 iterations; what is happening?
+
+    ## Most people find the results surprising, or even upsetting. In fact, the dramatic history surrounding this simple puzzle is fascinating.
+
+    ## This may tell us more about human psychology and decision making, but how do we get a handle on the probability?
+
+    ## One way to stretch our understanding is to increase the size of the board: what if we have more doors, and still reveal all but one of the bogus prizes?
+
+    """)
+    return
+
+
+@app.cell
+def _(game3, mo, step3):
+    mo.vstack([
+        step3,
+        mo.md('# More doors: Always change choice.'),
+        game3.step(),
+        mo.md('## Now it becomes clearer: when the selection is always changed, the contestant only loses if the intial guess was correct.  So the probability flips from $(1/N)$ to $(1 - 1/N)$!'),
     ])
     return
 
